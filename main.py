f28f2eb7e432237110594e50e322a50f0069abd0
Marius ROBERT <mariusrobert380@gmail.com>
2022-02-14 14:18:45 +0100

    update profs/matiere/option list

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from my_calendar import import_calendar, MyCalendar
 
-profs = {"M4101C": {"B": "MONTANVERT"}, "M4105C": {"B":"HAMON"}}#, "M4201C": "B", "M4202C": "D"}
+profs = {"M4101C": {"B": "MONTANVERT"},
+         "M4105C": {"B": "ORTEGA",
+                    "C":"DAILLY",
+                    "D": "LAURILLAU"},
+         "M4201C": {"C": "CHABOUD",
+                    "B": "BLANCO-LAINE"},
+         "M4202C": {"B": "HAMON",
+                    "D": "BILLIOT",
+                    "C": "CORSET"}
+         }
+lien_cal_S4B1 = "https://ade-uga.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=41515&projectId=1&calType=ical&firstDate=2022-02-14&lastDate=2022-04-15"
+lien_cal_S4B2 = "https://ade-uga.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=41526&projectId=1&calType=ical&firstDate=2022-02-14&lastDate=2022-04-15"
+lien_cal_S4C1 = "https://ade-uga.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=41815&projectId=1&calType=ical&firstDate=2022-02-14&lastDate=2022-04-15"
+lien_cal_S4C2 = "https://ade-uga.grenet.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=41816&projectId=1&calType=ical&firstDate=2022-02-14&lastDate=2022-04-15"
+
 
 def remove_options(calendar: MyCalendar, options: list) -> None:
     for event in calendar.events:
         if event.summary.split()[0] in options:
             calendar.events.remove(event)
+
 
 def get_cours_from_options(calendar: MyCalendar, options_dict: dict) -> None:
     global profs
@@ -14,10 +29,10 @@ def get_cours_from_options(calendar: MyCalendar, options_dict: dict) -> None:
             if event.description.find(profs[matiere][option]) != -1:
                 print(event.description)
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     matiere_options = ["M4101C", "M4105C", "M4201C", "M4202C"]
-    options_perso = {"M4101C": "B", "M4105C": "B"}# "M4201C": "M4201C", "M4202C": "M4202C"}
+    options_perso = {"M4101C": "B", "M4105C": "B"}  # "M4201C": "M4201C", "M4202C": "M4202C"}
 
     cal = import_calendar("ADECal.ics")
     # remove_options(cal, matiere_options)
